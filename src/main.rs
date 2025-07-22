6e8dd6ed77bd9506289cad8bb5b57ddcd92383b4
oriori-rust <senyassrcruzr@gmail.com>
2025-07-22 14:49:03 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,19 @@ fn restar(a: i32, b: i32) -> i32 {
     a - b
 }
 
+fn multiplicar(a: i32, b: i32) -> i32 {
+    a * b
+}
+
 fn main() {
     let a = 10;
     let b = 4;
 
     let resultado_suma = sumar(a, b);
     let resultado_resta = restar(a, b);
+    let resultado_multiplicar = multiplicar(a, b);
 
     println!("La suma de {} + {} es: {}", a, b, resultado_suma);
     println!("La resta de {} - {} es: {}", a, b, resultado_resta);
+    println!("La multiplicación de {} * {} es: {}", a, b, resultado_multiplicar);
 }
